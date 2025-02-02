e2182e9d0eab4b6e1f4c7a089d4e5dd4aebb938c
seongmo <leaveleave01@gmail.com>
2025-02-02 19:16:24 +0900

    debug rm

--- app.py
+++ app.py
@@ -140,5 +140,5 @@ def download_file(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
