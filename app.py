56b53783f3bad37af8d44cd75b385ae163e5be9f
seongmo <leaveleave01@gmail.com>
2025-02-02 17:27:49 +0900

    ip edit

--- app.py
+++ app.py
@@ -139,4 +139,6 @@ def download_file(filename):
     return send_from_directory(app.config['DOWNLOAD_FOLDER'], safe_filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
